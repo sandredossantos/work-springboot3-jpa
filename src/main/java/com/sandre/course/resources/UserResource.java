bd1a9e8246a972de9a9c668ac0960a91e759e817
Sandré Pereira <sandredossantos@hotmail.com>
2024-09-24 13:22:37 -0300

    h2 database

--- src/main/java/com/sandre/course/resources/UserResource.java
+++ src/main/java/com/sandre/course/resources/UserResource.java
@@ -8,11 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 import com.sandre.course.entities.User;
 
 @RestController
-@RequestMapping(value="/users")
+@RequestMapping(value = "/users")
 public class UserResource {
-	
+
 	@GetMapping
-	public ResponseEntity<User> findAll(){
+	public ResponseEntity<User> findAll() {
 		User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
 		return ResponseEntity.ok().body(u);
 	}

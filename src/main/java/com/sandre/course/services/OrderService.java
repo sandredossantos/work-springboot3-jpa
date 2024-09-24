057a5b9ecd8629caee8c2d358c4f23274f0435a8
Sandré Pereira <sandredossantos@hotmail.com>
2024-09-24 16:11:51 -0300

    relation p2

---
+++ src/main/java/com/sandre/course/services/OrderService.java
@@ -0,0 +1,27 @@
+package com.sandre.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sandre.course.entities.Order;
+import com.sandre.course.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository repository;
+
+	public List<Order> findAll() {
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}

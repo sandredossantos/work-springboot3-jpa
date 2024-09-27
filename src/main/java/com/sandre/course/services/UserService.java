69ef3674335c3d9ff8f1f5537328e8a3eaa6c3c1
Sandré Pereira <sandredossantos@hotmail.com>
2024-09-27 10:54:24 -0300

    exception

--- src/main/java/com/sandre/course/services/UserService.java
+++ src/main/java/com/sandre/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.sandre.course.entities.User;
 import com.sandre.course.repositories.UserRepository;
+import com.sandre.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -18,10 +19,30 @@ public class UserService {
 	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+	}
+
+	public User insert(User obj) {
+		return repository.save(obj);
+	}
+
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
+
+	public User update(Long id, User obj) {
+		User entity = repository.getReferenceById(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
 	}
 
 }
